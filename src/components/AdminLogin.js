f08aaf975bb148d0c105c6295a5dde518de86cd4
KowshikGajula <kowshikgajula1@gmail.com>
2024-12-10 21:02:57 +0530

    Finished Project, Frontend project deployed on Vercel and backend on Railway(remaining trial credits are .40 as of 10th December 2024)

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -9,7 +9,7 @@ function AdminLogin() {
 
     const handleAdminLogin = async () => {
         try {
-            const response = await fetch('http://localhost:8080/api/auth/login', {
+            const response = await fetch('https://indianculture-production.up.railway.app/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
